307784ca83ede71740ba62dfe02e6f44dedf2b3f
juno-choi <ililil9482@naver.com>
2022-04-26 20:56:33 +0900

    FIX : 에러코드 수정

--- src/main/java/com/juno/loginservice/controller/code/UserCode.java
+++ src/main/java/com/juno/loginservice/controller/code/UserCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserCode {
     BAD_REQUEST("400", "잘못된 입력값 존재", HttpStatus.BAD_REQUEST),
-    EXIST_USER("401", "이미 존재하는 아이디", HttpStatus.BAD_REQUEST),
+    EXIST_USER("403", "이미 존재하는 아이디", HttpStatus.FORBIDDEN),
     ;
     private String code;
     private String msg;
